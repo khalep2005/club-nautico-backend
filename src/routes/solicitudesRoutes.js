c3c7c4f819ad63cb22fc029a1b7e8301465113f9
Khalep <khalepvelarde123@gmail.com>
2026-05-14 14:19:29 -0500

    Se elimino un comentario que no aportaba nada al código, y se elimino una linea de codigo repetida.

--- src/routes/solicitudesRoutes.js
+++ src/routes/solicitudesRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { crearSolicitud, obtenerSolicitudes } = require('../controllers/solicitudesController');
+const { crearSolicitud, obtenerSolicitudes,evaluarSolicitud } = require('../controllers/solicitudesController');
 const { verificarToken, autorizarRoles } = require('../middlewares/authMiddleware');
-const { crearSolicitud, obtenerSolicitudes, evaluarSolicitud } = require('../controllers/solicitudesController');
 
 // RUTA GET: Listar todas las solicitudes (Jefe y Secretaría tienen acceso)
 router.get('/', verificarToken, autorizarRoles(1, 2), obtenerSolicitudes);
@@ -10,6 +9,7 @@ router.get('/', verificarToken, autorizarRoles(1, 2), obtenerSolicitudes);
 // RUTA POST: Crear nueva solicitud (SOLO Secretaría tiene acceso)
 router.post('/crear', verificarToken, autorizarRoles(2), crearSolicitud);
 
+// RUTA PUT: Evaluar solicitud (Aprobar/Rechazar) (SOLO Jefe tiene acceso)
 router.put('/:id/evaluar', verificarToken, autorizarRoles(1), evaluarSolicitud);
-    
+
 module.exports = router;
