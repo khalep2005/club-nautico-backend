5761544bc853dcbedc61e54e6620afd1773c42c0
cniki2651-arch <cniki2651@gmail.com>
2026-06-24 00:18:05 -0500

    cambios en authcontroller

--- src/routes/tripulantesRoutes.js
+++ src/routes/tripulantesRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { obtenerTripulantes, crearTripulante } = require('../controllers/tripulantesController');
 const { verificarToken, autorizarRoles } = require('../middlewares/authMiddleware');
 
-// Solo Naviero (Rol 3)
+// Solo Naviero 
 router.get('/', verificarToken, autorizarRoles(3), obtenerTripulantes);
 router.post('/crear', verificarToken, autorizarRoles(3), crearTripulante);
 
